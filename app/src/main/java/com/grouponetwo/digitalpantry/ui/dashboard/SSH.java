df4537e989f3e383020dbf46c7474cfb17a9f7ab
BillboEliasJohansson <elias.swe.jh@gmail.com>
2023-12-13 13:34:37 +0100

    Revamped and removed some functions from SSH class

--- app/src/main/java/com/grouponetwo/digitalpantry/ui/dashboard/SSH.java
+++ app/src/main/java/com/grouponetwo/digitalpantry/ui/dashboard/SSH.java
@@ -1,76 +1,47 @@
 package com.grouponetwo.digitalpantry.ui.dashboard;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 public class SSH {
-    private static StringBuilder output = new StringBuilder();
-
-    public static String executeSSHcommand(String command) {
+    private static final String USERNAME = "DP";
+    private static final String PASSWORD = "DP123";
+    private static final String IP_ADRESS = "192.168.1.126";
+    private static final int PORT = 22;
+    
+    public static void executeSSHcommand(String command) {
 
         new AsyncTask<Integer, Void, Void>(){
             @Override
             protected Void doInBackground(Integer... params) {
-                String user = "DP";
-                String password = "DP123";
-                String host = "192.168.1.126";
-                int port = 22;
 
                 try {
                     JSch jsch = new JSch();
-                    com.jcraft.jsch.Session session = jsch.getSession(user, host, port);
-                    session.setPassword(password);
-                    session.setConfig("StrictHostKeyChecking", "no");
-                    session.setTimeout(10000);
-                    session.connect();
-                    ChannelExec channel = (ChannelExec) session.openChannel("exec");
-                    channel.setCommand(command);
+                    com.jcraft.jsch.Session sess = jsch.getSession(USERNAME, IP_ADRESS, PORT);
+                    sess.setPassword(PASSWORD);
+                    sess.setConfig("StrictHostKeyChecking", "no");
+                    sess.setTimeout(10000);
+                    sess.connect();
+                    ChannelExec exec = (ChannelExec) sess.openChannel("exec");
+                    exec.setCommand(command);
+                    exec.connect();
+                    exec.disconnect();
+                    sess.disconnect();
 
-                    // Capture the output
-                    InputStream commandOutput = channel.getInputStream();
-
-                    channel.connect();
-
-                    // Read the output
-                    byte[] tmp = new byte[1024];
-                    while (true) {
-                        while (commandOutput.available() > 0) {
-                            int i = commandOutput.read(tmp, 0, 1024);
-                            if (i < 0) break;
-                            output.append(new String(tmp, 0, i));
-                        }
-
-                        if (channel.isClosed()) {
-                            if (commandOutput.available() > 0) continue;
-                            break;
-                        }
-
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException ignored) {
-                        }
-                    }
-
-                    channel.disconnect();
-                    session.disconnect();
-
-                } catch (JSchException | IOException e) {
-
+                } catch (JSchException e) {
+                    Log.i("SSH", "Failed");
                 }
                 return null;
             }
             @Override
             protected void onPostExecute(Void v) {
-
+                Log.i("SSH", "Success");
             }
         }.execute(1);
 
-        return output.toString();
     }
 }
