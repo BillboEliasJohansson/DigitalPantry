c5de1e9066b4e5172412bdde3d9b867610d7c21f
BillboEliasJohansson <elias.swe.jh@gmail.com>
2023-12-20 12:24:27 +0100

    Added UI element to active voice control script

--- app/src/main/java/com/grouponetwo/digitalpantry/ui/home/HomeFragment.java
+++ app/src/main/java/com/grouponetwo/digitalpantry/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.grouponetwo.digitalpantry.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.grouponetwo.digitalpantry.databinding.FragmentHomeBinding;
 import com.grouponetwo.digitalpantry.ui.dashboard.SSH;
 
@@ -17,17 +19,28 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    private FloatingActionButton voice;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         TextView textView = binding.stockTextView;
+        voice = binding.VoiceControl;
+
+        voice.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SSH.executeSSHcommand("python VoiceControl.py");
+            }
+        });
 
         SSH.executeSSHcommand("python PrintList.py", textView);
 
         return root;
     }
+
+
 
     @Override
     public void onDestroyView() {
